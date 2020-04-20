0c7774c505266e99706e6d611fd7dd0fc1d47250
vtarasich <chisarat@gmail.com>
2020-04-20 16:59:13 +0300

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,44 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "google"
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "extends": [
+    "google",
+    "react-hooks",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+  },
+  "plugins": [
+    "@typescript-eslint",
+  ],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": [
+      2,
+      {
+        "args": "none",
+      },
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    "max-len": ["error", 120],
+    "require-jsdoc": "off",
+    "indent": ["error", 2],
+    "object-curly-spacing": ["error", "always"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  "overrides": [
+    {
+      "files": ["**/*.tsx"],
+      "rules": {
+        "react/prop-types": "off",
+      },
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "max-len": ["error", 120],
-        "require-jsdoc": "off",
-        "indent": ["error", 4],
-        "object-curly-spacing": ["error", "always"],
-    }
+  ],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { coinToss } from './app/SampleModule';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = ['Coin', 'drops at', coinToss()].join(' ');
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = ['Coin', 'drops at', coinToss()].join(' ');
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
